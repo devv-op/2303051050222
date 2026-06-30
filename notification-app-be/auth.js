3417974d5d6cde83eeba316f0cde83a8a6a67892
devv-op <devpatel3145@gmail.com>
2026-06-30 15:19:55 +0530

    updated

--- notification-app-be/auth.js
+++ notification-app-be/auth.js
@@ -1,14 +1,14 @@
-import { BASE_URL, credentials } from "./api";
-import { setToken } from "./logger";
+import { BASE_URL, credentials } from "./config.js";
+import { setToken } from "./logger.js";
 
 export async function login() {
 
     const response = await fetch(`${BASE_URL}/auth`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify(credentials),
+        body: JSON.stringify(credentials)
     });
 
     const data = await response.json();
