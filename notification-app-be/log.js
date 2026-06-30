ee6066dd92772fc32a629d78be3f5f2bc2b50d16
devv-op <devpatel3145@gmail.com>
2026-06-30 15:14:53 +0530

    update

---
+++ notification-app-be/log.js
@@ -0,0 +1,23 @@
+import { BASE_URL } from "./config.js";
+
+let token = "";
+
+export function setToken(newToken) {
+    token = newToken;
+}
+
+export async function Log(stack, level, packageName, message) {
+    await fetch(`${BASE_URL}/logs`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            stack,
+            level,
+            package: packageName,
+            message
+        })
+    });
+}
