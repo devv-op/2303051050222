0a89f52ffecb260fc16b5f2e75b9fd4448af7a49
devv-op <devpatel3145@gmail.com>
2026-06-30 15:08:18 +0530

    update

---
+++ notification-app-be/config.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://4.224.186.213/evaluation-service";
+
+export const credentials = {
+    email: "2303051050222@paruluniversity.ac.in",
+    name: "devvrat patel",
+    rollNo: "2303051050222",
+    accessCode: "cJqaEB",
+    clientID: "336343e7-08e2-477f-af14-aa618fb47f7c",
+    clientSecret: "rJkTQrrCJwvTbGWm"
+};
